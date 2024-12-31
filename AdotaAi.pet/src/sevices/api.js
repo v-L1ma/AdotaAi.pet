cb85ec8e22ee9cf72a07868842cba3508360e4db
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2024-12-31 17:27:41 -0300

    tentando conectar com o backend

--- AdotaAi.pet/src/sevices/api.js
+++ AdotaAi.pet/src/sevices/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://backend-adota-ai-pet.vercel.app'
+    baseURL: 'https://backend-adota-ai-pet-2tv4.vercel.app'
 })
 
 export default api
