5b73deb5eb23d22c7c35b27da69c0cc193a0013b
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-08-23 13:04:07 -0300

    adicionando a estrutura do backend

---
+++ backend/src/main/java/com/adotaai/adotaai/AdotaaiApplication.java
@@ -0,0 +1,13 @@
+package com.adotaai.adotaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdotaaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdotaaiApplication.class, args);
+	}
+
+}
