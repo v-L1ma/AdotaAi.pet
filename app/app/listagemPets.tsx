2affba4ce1f27a006e4e68636c8f03a5e3d8cbef
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2025-08-26 18:28:00 -0300

    feat: criando navegação para a pagina de listagem

---
+++ app/app/listagemPets.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function ListagemPets(){
+    return(
+        <View>
+            <Text>io</Text>
+        </View>
+    )
+}
--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -2,12 +2,16 @@ import { StyleSheet, View } from "react-native";
 import Icon from 'react-native-vector-icons/Octicons';
 import IconMat from 'react-native-vector-icons/MaterialCommunityIcons';
 import { colors } from "@/styles/variables";
+import { useRouter } from "expo-router";
 
 export default function NavBar(){
+
+    const router = useRouter();
+
     return(
         <View style={styles.container}>
             <Icon style={styles.icon} name="home" size={30} color={colors.primary}></Icon>
-            <IconMat style={styles.icon} name="cards-heart-outline" size={30} color={colors.primary}></IconMat>
+            <IconMat onPress={()=>router.push("/listagemPets")} style={styles.icon} name="cards-heart-outline" size={30} color={colors.primary}></IconMat>
             
             <IconMat style={styles.icon} name="heart-plus-outline" size={30} color={colors.primary}></IconMat>
             <IconMat style={styles.icon} name="menu" size={30} color={colors.primary}></IconMat>
