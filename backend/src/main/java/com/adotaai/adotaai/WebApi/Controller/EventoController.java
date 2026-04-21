7880166b370c0a3d46a32a531386e596c5d3513b
Vinicius Lima <102876820+v-L1ma@users.noreply.github.com>
2026-04-21 15:48:21 -0300

    fix: arrumando controller eventos

--- backend/src/main/java/com/adotaai/adotaai/WebApi/Controller/EventoController.java
+++ backend/src/main/java/com/adotaai/adotaai/WebApi/Controller/EventoController.java
@@ -1,14 +1,21 @@
 package com.adotaai.adotaai.WebApi.Controller;
 
-import com.adotaai.adotaai.Application.DTO.EventoDTO;
-import com.adotaai.adotaai.Application.DTO.PetDTO;
-import com.adotaai.adotaai.Application.Service.EventoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.util.List;
 import java.util.UUID;
 
-import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adotaai.adotaai.Application.DTO.EventoDTO;
+import com.adotaai.adotaai.Application.Service.EventoService;
 
 @RestController
 @RequestMapping(value = "/eventos")
